fc6fa8dff1d758f8ca6ece95b7b3e5a74de0b56a
2204happy <benbartley2@gmail.com>
2018-07-16 15:06:37 +1000

    init

---
+++ Assets/scripts/playerCtl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCtl : MonoBehaviour {
+	private GameObject objSelf;
+
+	// Use this for initialization
+	void Start () {
+		objSelf = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)) {
+			objSelf.GetComponent<vehicleCtl>().accelerate();
+		}
+		if (Input.GetKey(KeyCode.S))
+        {
+            objSelf.GetComponent<vehicleCtl>().brake();
+        }
+		if (Input.GetKey(KeyCode.A))
+        {
+			objSelf.GetComponent<vehicleCtl>().turnLeft();
+        }
+		if (Input.GetKey(KeyCode.D))
+        {
+			objSelf.GetComponent<vehicleCtl>().turnRight();
+        }
+	}
+}
---
+++ Assets/scripts/vehicleCtl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehicleCtl : MonoBehaviour {
+	private GameObject objSelf;
+	public float turnModif;
+	public float moveModif;
+	public float friction;
+	public float topSpeed;
+	public float acceleration;
+	public float brakeSpeed;
+	public GameObject childObj;
+	private float speed;
+	// Use this for initialization
+	void Start () {
+		objSelf = this.gameObject;
+		speed = 0;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (speed > topSpeed) {
+			speed = topSpeed;
+		}
+		if (speed < 0) {
+			speed = 0;
+		}
+        
+        Debug.Log(speed);
+		childObj.transform.localPosition = new Vector3(0, speed, 0);
+		objSelf.transform.position = childObj.transform.position;
+		speed -= friction;
+	}
+	public void turnLeft() {
+		objSelf.transform.Rotate(new Vector3(0,0,turnModif));
+	}
+
+    public void turnRight()
+    {
+        objSelf.transform.Rotate(new Vector3(0, 0, -turnModif));
+    }
+	public void accelerate() {
+		speed += acceleration;
+	}
+	public void brake() {
+		speed -= brakeSpeed;
+	}
+}
