5e8aa75b712568ecc9dbf4fd7e7ce2d12eb569a4
2204happy <benbartley2@gmail.com>
2018-07-26 09:10:29 +1000

    raycast stuff

--- Assets/scripts/raycast.cs
+++ Assets/scripts/raycast.cs
@@ -6,11 +6,14 @@ public class raycast : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Physics2D.queriesHitTriggers = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+		Debug.Log(hit.collider);
+		Debug.Log(hit.distance);
+		Debug.DrawLine(transform.position, Vector2.up * hit.distance,Color.white);
 	}
 }
