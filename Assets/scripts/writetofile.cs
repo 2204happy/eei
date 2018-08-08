6a998f8b077ab056e9968fadc711d6751614298b
EnderJ13 <james@pringles.net>
2018-08-08 12:28:27 +1000

    added write to file script

---
+++ Assets/scripts/writetofile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class writetofile : MonoBehaviour {
+
+    public static string write(string _input, string _file_path = "/Files/Output.txt")
+    {
+        using (StreamWriter outputFile = new StreamWriter(Application.dataPath + _file_path))
+        {
+            outputFile.WriteLine(_input);
+        }
+        return _input;
+    }
+
+    public static string append(string _input, string _file_path = "/Files/Output.txt")
+    {
+        using (StreamWriter outputFile = new StreamWriter(Application.dataPath + _file_path, true))
+        {
+            outputFile.WriteLine(_input);
+        }
+        return _input;
+    }
+}
